cc5d16d1759e2ad57fcf003d5865a9dc17763374
Clark Sell <clark.sell@csell.net>
2019-12-27 08:55:26 -0600

    adds auth to mutations and builds user context

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,14 +1,20 @@
+import _ from 'lodash';
 import {
   ApolloServer,
   gql,
   addMockFunctionsToSchema,
+  mergeSchemas,
 } from 'apollo-server-cloud-functions';
+import debug from 'debug';
 import { buildFederatedSchema } from '@apollo/federation';
-// import { addMockFunctionsToSchema } from 'graphql-tools';
+import { security } from '@thatconference/api';
 
 // Graph Types and Resolvers
 import typeDefsRaw from './typeDefs';
 import resolvers from './resolvers';
+import directives from './directives';
+
+const dlog = debug('that:api:sessions:graphServer');
 
 // convert our raw schema to gql
 const typeDefs = gql`
@@ -25,21 +31,28 @@ const typeDefs = gql`
  *     createGateway(userContext)
  */
 const createServer = ({ dataSources }, enableMocking = false) => {
-  let schema = {};
+  let federatedSchemas = {};
   const { logger } = dataSources;
 
   if (!enableMocking) {
-    schema = buildFederatedSchema([{ typeDefs, resolvers }]);
+    federatedSchemas = buildFederatedSchema([{ typeDefs, resolvers }]);
   } else {
-    schema = buildFederatedSchema([{ typeDefs }]);
+    federatedSchemas = buildFederatedSchema([{ typeDefs }]);
 
     addMockFunctionsToSchema({
-      schema,
+      federatedSchemas,
       // eslint-disable-next-line global-require
       mocks: require('./__mocks__').default(),
       preserveResolvers: true, // so GetServiceDefinition works
     });
   }
+
+  const schema = mergeSchemas({
+    schemas: [federatedSchemas],
+    schemaDirectives: {
+      ...directives,
+    },
+  });
 
   return new ApolloServer({
     schemaDirectives: {},
@@ -49,15 +62,31 @@ const createServer = ({ dataSources }, enableMocking = false) => {
       ? { endpoint: '/' }
       : false,
 
-    // todo: here is where we build up our dataSources for our resolvers to use later.
     dataSources: () => ({
       ...dataSources,
     }),
 
-    // todo: here is where we will build our users context that our resolvers can leverage later.
-    context: async ({ req }) => ({
-      ...(req && req.context),
-    }),
+    context: async ({ req, res }) => {
+      dlog('buulding graphql user context');
+      let context = {};
+
+      dlog('auth header %o', req.headers);
+      if (!_.isNil(req.headers.authorization)) {
+        dlog('validating token for %o:', req.headers.authorization);
+
+        const validatedToken = await security
+          .jwt()
+          .verify(req.headers.authorization);
+
+        dlog('validated token: %o', validatedToken);
+        context = {
+          ...context,
+          user: validatedToken,
+        };
+      }
+
+      return context;
+    },
 
     formatError: err => {
       logger.warn('graphql error', err);
