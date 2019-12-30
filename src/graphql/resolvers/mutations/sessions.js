3e21c3b510b4d818196b0fbac964d43365061b90
Brett Slaski <brett@thatconference.com>
2019-12-30 14:19:03 -0600

    add transaction emails + reformat version

--- src/graphql/resolvers/mutations/sessions.js
+++ src/graphql/resolvers/mutations/sessions.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import debug from 'debug';
+import moment from 'moment';
 import sessionStore from '../../../dataSources/cloudFirestore/session';
 
 const dlog = debug('that:api:sessions:mutation');
@@ -13,12 +14,40 @@ export const fieldResolvers = {
     ) => {
       dlog('SessionsMutation:create called');
 
-      // todo: call out to postmark
       const newSession = await sessionStore(firestore, logger).create({
         eventId,
         user,
         session,
       });
+
+      // TODO: provide object with member's information
+      // await postmark.sendEmailWithTemplate({
+      //   // TemplateId: 15581327,
+      //   TemplateAlias: 'THATconferenceSessionCreated',
+      //   From: 'hello@thatconference.com',
+      //   To: 'Users-email-address',
+      //   TemplateModel: {
+      //     member: {
+      //       firstName: 'first-name',
+      //       lastName: 'last-name',
+      //     },
+      //     session: {
+      //       id: newSession.id,
+      //       title: newSession.title,
+      //       createdAt: moment(newSession.createdAt).format(
+      //         'M/D/YYYY h:mm:ss A',
+      //       ),
+      //     },
+      //     // Optional (hard coded in email now)
+      //     event: {
+      //       name: 'Events name',
+      //       year: 'Event year',
+      //       cfpOpens: 'CallForCounselorOpenDate',
+      //       cfpCloses: 'CallForCounselorCloseDate',
+      //       announceDate: 'scheduleAnnouncedDate',
+      //     },
+      //   },
+      // });
 
       return newSession;
     },
