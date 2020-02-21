99e2396e51bee7b3d944768c88213a1935484548
Clark Sell <clark.sell@csell.net>
2020-02-21 08:43:53 -0600

    adds request tracking and updates async

--- src/events/user.js
+++ src/events/user.js
@@ -11,63 +11,73 @@ function userEvents(postmark) {
   function newSessionCreated({ user, session }) {
     dlog('new session created');
 
-    postmark.sendEmailWithTemplate({
-      // TemplateId: 15581327,
-      TemplateAlias: 'THATconferenceSessionCreated',
-      From: 'Hello@THATConference.com',
-      To: user.email,
-      TemplateModel: {
-        member: {
-          firstName: user.firstName,
-          lastName: user.lastName,
+    return postmark
+      .sendEmailWithTemplate({
+        // TemplateId: 15581327,
+        TemplateAlias: 'THATconferenceSessionCreated',
+        From: 'Hello@THATConference.com',
+        To: user.email,
+        TemplateModel: {
+          member: {
+            firstName: user.firstName,
+            lastName: user.lastName,
+          },
+          session: {
+            id: session.id,
+            title: session.title,
+            createdAt: moment(session.createdAt).format('M/D/YYYY h:mm:ss A'),
+          },
+          // Optional (hard coded in email now)
+          // event: {
+          //   name: 'Events name',
+          //   year: 'Event year',
+          //   cfpOpens: 'CallForCounselorOpenDate',
+          //   cfpCloses: 'CallForCounselorCloseDate',
+          //   announceDate: 'scheduleAnnouncedDate',
+          // },
         },
-        session: {
-          id: session.id,
-          title: session.title,
-          createdAt: moment(session.createdAt).format('M/D/YYYY h:mm:ss A'),
-        },
-        // Optional (hard coded in email now)
-        // event: {
-        //   name: 'Events name',
-        //   year: 'Event year',
-        //   cfpOpens: 'CallForCounselorOpenDate',
-        //   cfpCloses: 'CallForCounselorCloseDate',
-        //   announceDate: 'scheduleAnnouncedDate',
-        // },
-      },
-    });
+      })
+      .then(dlog('email sent'))
+      .catch(e => process.nextTick(() => userEventEmitter.emit('error', e)));
   }
 
   function sessionUpdated({ user, session }) {
-    postmark.sendEmailWithTemplate({
-      // TemplateId: 15581957,
-      TemplateAlias: 'THATconferenceSessionUpdated',
-      From: 'Hello@THATConference.com',
-      To: user.email,
-      TemplateModel: {
-        member: {
-          firstName: user.firstName,
-          lastName: user.lastName,
-        },
-        session: {
-          id: session.id,
-          title: session.title,
-          lastUpdatedAt: moment(session.lastUpdatedAt).format(
-            'M/D/YYYY h:mm:ss A',
-          ),
-        },
-        // Optional (hard coded in email now)
-        // event: {
-        //   name: 'Events name',
-        //   year: 'Event year',
-        //   cfpOpens: 'CallForCounselorOpenDate',
-        //   cfpCloses: 'CallForCounselorCloseDate',
-        //   announceDate: 'scheduleAnnouncedDate',
-        // },
-      },
-    });
     dlog('session updated event fired');
+    return postmark
+      .sendEmailWithTemplate({
+        // TemplateId: 15581957,
+        TemplateAlias: 'THATconferenceSessionUpdated',
+        From: 'Hello@THATConference.com',
+        To: user.email,
+        TemplateModel: {
+          member: {
+            firstName: user.firstName,
+            lastName: user.lastName,
+          },
+          session: {
+            id: session.id,
+            title: session.title,
+            lastUpdatedAt: moment(session.lastUpdatedAt).format(
+              'M/D/YYYY h:mm:ss A',
+            ),
+          },
+          // Optional (hard coded in email now)
+          // event: {
+          //   name: 'Events name',
+          //   year: 'Event year',
+          //   cfpOpens: 'CallForCounselorOpenDate',
+          //   cfpCloses: 'CallForCounselorCloseDate',
+          //   announceDate: 'scheduleAnnouncedDate',
+          // },
+        },
+      })
+      .then(dlog('email sent'))
+      .catch(e => process.nextTick(() => userEventEmitter.emit('error', e)));
   }
+
+  userEventEmitter.on('error', err => {
+    throw new Error(err);
+  });
 
   userEventEmitter.on('newSessionCreated', newSessionCreated);
   userEventEmitter.on('sessionUpdated', sessionUpdated);
