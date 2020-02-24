a541b5e4977b962adfa81399180e5deae922df7e
Brett Slaski <brett@thatconference.com>
2020-02-24 16:11:03 -0600

    add query to get ACCEPTED session by slug (sessionSlug) [ch1942]

--- src/dataSources/cloudFirestore/session.js
+++ src/dataSources/cloudFirestore/session.js
@@ -72,6 +72,25 @@ function sessions(dbInstance) {
     return result;
   }
 
+  async function findSessionBySlug(slug) {
+    const docSnap = await sessionsCol
+      .where('sessionSlug', '==', slug.toLowerCase())
+      .where('status', '==', 'ACCEPTED')
+      .get();
+
+    let results = null;
+
+    if (docSnap.size === 1) {
+      dlog('found session by slug %o', slug);
+      const session = docSnap.docs[0].data();
+      session.id = docSnap.docs[0].id;
+
+      results = session;
+    }
+
+    return results;
+  }
+
   async function batchFindSessions(sessionIds) {
     dlog('batchFindSessions %o', sessionIds);
 
@@ -125,6 +144,7 @@ function sessions(dbInstance) {
     update,
     findMy,
     findMySession,
+    findSessionBySlug,
     batchFindSessions,
     getTotalProfessionalSubmittedForEvent,
   };
