4a09c03e96d5a105441289ada83998b7ba282ee4
Brett Slaski <brett@thatconference.com>
2020-08-12 09:14:46 -0500

    Add get accepted session by id

--- src/graphql/resolvers/queries/sessions.js
+++ src/graphql/resolvers/queries/sessions.js
@@ -1,14 +1,19 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/prefer-default-export */
 import debug from 'debug';
+import sessionStore from '../../../dataSources/cloudFirestore/session';
 
 const dlog = debug('that:api:sessions:query:SessionsQuery');
 
 export const fieldResolvers = {
   SessionsQuery: {
-    me: (parent, args, { dataSources: { firestore }, user }) => {
+    me: () => {
       dlog('me called');
       return {};
+    },
+    session: (_, { sessionId }, { dataSources: { firestore } }) => {
+      dlog('session called');
+      return sessionStore(firestore).findAcceptedSession(sessionId);
     },
   },
 };
