ead4bdc5199c34269c812a094ac34a6445a89eef
Brett Slaski <brett@thatconference.com>
2022-01-03 16:33:15 -0600

    Fixes og-image issue on non-approved sessions

--- src/events/user.js
+++ src/events/user.js
@@ -292,6 +292,11 @@ function userEvents(postmark) {
 
   function setOgImage({ session }) {
     dlog('call setOgImage');
+    if (session.status !== 'ACCEPTED') {
+      dlog('session not accepted, leaving setOgImage');
+      return;
+    }
+
     callOgImage(session.id)
       .then(res => dlog('setOgImage result: %o', res))
       .catch(e => process.nextTick(() => userEventEmitter.emit('error', e)));
