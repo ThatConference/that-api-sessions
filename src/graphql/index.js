049c535d88f34b9db7d02d1e72a985b2f7046f8c
Brett Slaski <brett@thatconference.com>
2023-04-07 12:02:03 -0500

    fix sentry logging

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -72,10 +72,9 @@ const createServerParts = ({ dataSources, httpServer }) => {
       Sentry.withScope(scope => {
         scope.setTag('formatError', true);
         scope.setLevel('warning');
-
-        scope.setExtra('originalError', err.originalError);
-        scope.setExtra('path', err.path);
-
+        scope.setContext('originalError', { originalError: err.originalError });
+        scope.setContext('path', { path: err.path });
+        scope.setContext('error object', { error: err });
         Sentry.captureException(err);
       });
 
